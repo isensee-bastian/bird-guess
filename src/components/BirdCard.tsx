56281ea3949b839092045edb23bbf01858cb5039
Bastian Isensee <bastian-isensee@gmx.de>
2023-01-29 15:51:30 +0100

    Image and sound test including android

--- src/components/BirdCard.tsx
+++ src/components/BirdCard.tsx
@@ -1,7 +1,7 @@
 import './BirdCard.css';
 
 import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 
 
 interface BirdCardProps {
@@ -9,16 +9,21 @@ interface BirdCardProps {
 }
 
 const BirdCard: React.FC<BirdCardProps> = ({ }) => {
+    const playSound = () => {
+        const sound = new Audio("assets/birds/pileated_woodpecker.mp3");
+        sound.play();
+    };
+
     return (
         <IonCard>
-            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <img alt="Bird image" src="assets/birds/pileated_woodpecker.jpg" height="300" />
             <IonCardHeader>
-                <IonCardTitle>Card Title</IonCardTitle>
-                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                <IonCardTitle>Pileated Woodpecker</IonCardTitle>
             </IonCardHeader>
-
             <IonCardContent>
-                Here's a small text description for the card content. Nothing more, nothing less.
+                <p>Image: Shenandoah National Park from Virginia, Public domain, via Wikimedia Commons, https://commons.wikimedia.org/wiki/File:Pileated_Woodpecker_(9597212081),_crop.jpg</p>
+                <p>Sound: Russ Wigh, XC649554. Accessible at www.xeno-canto.org/649554. Creative Commons Attribution-NonCommercial-ShareAlike 4.0</p>
+                <IonButton onClick={() => playSound()}>Play Sound</IonButton>
             </IonCardContent>
         </IonCard>
     );
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import BirdCard from '../components/BirdCard';
 
@@ -18,7 +17,6 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
-        <BirdCard />
         <BirdCard />
 
       </IonContent>
